ea03608a357bdc524db195674d455bcb72003523
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2022-03-17 11:21:07 +0100

    improve error logging

--- cmd/gardener-extension-provider-gcp/app/app.go
+++ cmd/gardener-extension-provider-gcp/app/app.go
@@ -146,37 +146,37 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if err := aggOption.Complete(); err != nil {
-				return fmt.Errorf("error completing options")
+				return fmt.Errorf("error completing options: %w", err)
 			}
 
 			util.ApplyClientConnectionConfigurationToRESTConfig(configFileOpts.Completed().Config.ClientConnection, restOpts.Completed().Config)
 
 			if workerReconcileOpts.Completed().DeployCRDs {
 				if err := worker.ApplyMachineResourcesForConfig(ctx, restOpts.Completed().Config); err != nil {
-					return fmt.Errorf("error ensuring the machine CRDs")
+					return fmt.Errorf("error ensuring the machine CRDs: %w", err)
 				}
 			}
 
 			mgr, err := manager.New(restOpts.Completed().Config, mgrOpts.Completed().Options())
 			if err != nil {
-				return fmt.Errorf("could not instantiate manager")
+				return fmt.Errorf("could not instantiate manager: %w", err)
 			}
 
 			scheme := mgr.GetScheme()
 			if err := controller.AddToScheme(scheme); err != nil {
-				return fmt.Errorf("could not update manager scheme")
+				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 			if err := gcpinstall.AddToScheme(scheme); err != nil {
-				return fmt.Errorf("could not update manager scheme")
+				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 			if err := druidv1alpha1.AddToScheme(scheme); err != nil {
-				return fmt.Errorf("could not update manager scheme")
+				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 			if err := autoscalingv1beta2.AddToScheme(scheme); err != nil {
-				return fmt.Errorf("could not update manager scheme")
+				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 			if err := machinev1alpha1.AddToScheme(scheme); err != nil {
-				return fmt.Errorf("could not update manager scheme")
+				return fmt.Errorf("could not update manager scheme: %w", err)
 			}
 
 			// add common meta types to schema for controller-runtime to use v1.ListOptions
@@ -184,14 +184,14 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 
 			useTokenRequestor, err := controller.UseTokenRequestor(generalOpts.Completed().GardenerVersion)
 			if err != nil {
-				return fmt.Errorf("could not determine whether token requestor should be used")
+				return fmt.Errorf("could not determine whether token requestor should be used: %w", err)
 			}
 			gcpcontrolplane.DefaultAddOptions.UseTokenRequestor = useTokenRequestor
 			gcpworker.DefaultAddOptions.UseTokenRequestor = useTokenRequestor
 
 			useProjectedTokenMount, err := controller.UseServiceAccountTokenVolumeProjection(generalOpts.Completed().GardenerVersion)
 			if err != nil {
-				return fmt.Errorf("could not determine whether service account token volume projection should be used")
+				return fmt.Errorf("could not determine whether service account token volume projection should be used: %w", err)
 			}
 			gcpcontrolplane.DefaultAddOptions.UseProjectedTokenMount = useProjectedTokenMount
 			gcpinfrastructure.DefaultAddOptions.UseProjectedTokenMount = useProjectedTokenMount
@@ -214,15 +214,15 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 			workerCtrlOpts.Completed().Apply(&gcpworker.DefaultAddOptions.Controller)
 
 			if _, _, err := webhookOptions.Completed().AddToManager(ctx, mgr); err != nil {
-				return fmt.Errorf("could not add webhooks to manager")
+				return fmt.Errorf("could not add webhooks to manager: %w", err)
 			}
 
 			if err := controllerSwitches.Completed().AddToManager(mgr); err != nil {
-				return fmt.Errorf("could not add controllers to manager")
+				return fmt.Errorf("could not add controllers to manager: %w", err)
 			}
 
 			if err := mgr.Start(ctx); err != nil {
-				return fmt.Errorf("error running manager")
+				return fmt.Errorf("error running manager: %w", err)
 			}
 
 			return nil
