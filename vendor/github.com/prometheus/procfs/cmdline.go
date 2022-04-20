ca6788fe821e1caf9e804bb0f272817de08e0acc
Himanshu Sharma <himanshu.sharma10@sap.com>
2022-04-20 14:00:49 +0530

    vendor mcm 0.44.1

--- vendor/github.com/prometheus/procfs/cmdline.go
+++ vendor/github.com/prometheus/procfs/cmdline.go
@@ -1,10 +1,9 @@
-// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
-//
+// Copyright 2021 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,5 +11,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This package imports CI related scripts - it is to force `go mod` to see them as dependencies.
-package ci
+package procfs
+
+import (
+	"strings"
+
+	"github.com/prometheus/procfs/internal/util"
+)
+
+// CmdLine returns the command line of the kernel.
+func (fs FS) CmdLine() ([]string, error) {
+	data, err := util.ReadFileNoStat(fs.proc.Path("cmdline"))
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Fields(string(data)), nil
+}
