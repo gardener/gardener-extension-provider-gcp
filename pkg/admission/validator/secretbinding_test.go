31c5523830f3ccc581aa5c65558b04a25aa081a3
Shafeeque E S <shafeeque.e.s@sap.com>
2022-10-06 15:45:53 +0530

    Vendor `gardener@1.56.0` and Upgrade k8s dependencies and controller-runtime (#496)
    * Vendor gardener@v1.56.0
    
    Upgrade `k8s.io/*` dependencies and controller-runtime
    
    * Adapt client Get call with GetOption
    
    * Run `make generate`
    
    * Add QPS and Burst to HealthCheck config
    
    These values are from example/00-componentconfig.yaml
    
    * Edit 10-fake-shoot-controlplane.yaml
    
    * Change container registry from k8s.gcr.io to registry.k8s.io

--- pkg/admission/validator/secretbinding_test.go
+++ pkg/admission/validator/secretbinding_test.go
@@ -88,7 +88,7 @@ var _ = Describe("SecretBinding validator", func() {
 
 		It("should return err when the corresponding Secret is not valid", func() {
 			apiReader.EXPECT().Get(context.TODO(), client.ObjectKey{Namespace: namespace, Name: name}, gomock.AssignableToTypeOf(&corev1.Secret{})).
-				DoAndReturn(func(_ context.Context, _ client.ObjectKey, obj *corev1.Secret) error {
+				DoAndReturn(func(_ context.Context, _ client.ObjectKey, obj *corev1.Secret, _ ...client.GetOption) error {
 					secret := &corev1.Secret{Data: map[string][]byte{
 						"foo": []byte("bar"),
 					}}
@@ -102,7 +102,7 @@ var _ = Describe("SecretBinding validator", func() {
 
 		It("should return nil when the corresponding Secret is valid", func() {
 			apiReader.EXPECT().Get(context.TODO(), client.ObjectKey{Namespace: namespace, Name: name}, gomock.AssignableToTypeOf(&corev1.Secret{})).
-				DoAndReturn(func(_ context.Context, _ client.ObjectKey, obj *corev1.Secret) error {
+				DoAndReturn(func(_ context.Context, _ client.ObjectKey, obj *corev1.Secret, _ ...client.GetOption) error {
 					secret := &corev1.Secret{Data: map[string][]byte{
 						gcp.ServiceAccountJSONField: []byte(`{"project_id": "project"}`),
 					}}
