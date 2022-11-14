92066396fb1287bd33f4a4a3133c699b452af7d6
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2022-11-14 19:57:40 +0100

    fix master tests

--- pkg/controller/bastion/configvalidator_test.go
+++ pkg/controller/bastion/configvalidator_test.go
@@ -108,8 +108,8 @@ var _ = Describe("ConfigValidator", func() {
 		BeforeEach(func() {
 			cluster = createClusters()
 			key := client.ObjectKey{Namespace: cluster.ObjectMeta.Name, Name: cluster.Shoot.Name}
-			c.EXPECT().Get(ctx, key, gomock.AssignableToTypeOf(&extensionsv1alpha1.Worker{})).DoAndReturn(
-				func(_ context.Context, namespacedName client.ObjectKey, obj *extensionsv1alpha1.Worker) error {
+			c.EXPECT().Get(ctx, key, &extensionsv1alpha1.Worker{}).DoAndReturn(
+				func(_ context.Context, namespacedName client.ObjectKey, obj *extensionsv1alpha1.Worker, _ ...client.GetOption) error {
 					worker.DeepCopyInto(obj)
 					return nil
 				})
