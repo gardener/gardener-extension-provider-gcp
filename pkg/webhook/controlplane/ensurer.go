0ef6eed7e8559c53bda1c8f267da69ed18580a77
Rafael Franzke <rafael.franzke@sap.com>
2023-06-21 13:42:41 +0200

    Adapt `controlplane` webhook

--- pkg/webhook/controlplane/ensurer.go
+++ pkg/webhook/controlplane/ensurer.go
@@ -25,17 +25,20 @@ import (
 	gcontext "github.com/gardener/gardener/extensions/pkg/webhook/context"
 	"github.com/gardener/gardener/extensions/pkg/webhook/controlplane/genericmutator"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	"github.com/gardener/gardener/pkg/component/machinecontrollermanager"
 	gutil "github.com/gardener/gardener/pkg/utils/gardener"
 	versionutils "github.com/gardener/gardener/pkg/utils/version"
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	vpaautoscalingv1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 	"k8s.io/utils/pointer"
 
 	"github.com/gardener/gardener-extension-provider-gcp/pkg/gcp"
+	"github.com/gardener/gardener-extension-provider-gcp/pkg/internal/imagevector"
 )
 
 // NewEnsurer creates a new controlplane ensurer.
@@ -52,11 +55,24 @@ type ensurer struct {
 	gardenletManagesMCM bool
 }
 
+// ImageVector is exposed for testing.
+var ImageVector = imagevector.ImageVector()
+
 // EnsureMachineControllerManagerDeployment ensures that the machine-controller-manager deployment conforms to the provider requirements.
 func (e *ensurer) EnsureMachineControllerManagerDeployment(_ context.Context, _ gcontext.GardenContext, newObj, _ *appsv1.Deployment) error {
 	if !e.gardenletManagesMCM {
 		return nil
 	}
+
+	image, err := ImageVector.FindImage(gcp.MachineControllerManagerProviderGCPImageName)
+	if err != nil {
+		return err
+	}
+
+	newObj.Spec.Template.Spec.Containers = extensionswebhook.EnsureContainerWithName(
+		newObj.Spec.Template.Spec.Containers,
+		machinecontrollermanager.ProviderSidecarContainer(newObj.Namespace, gcp.Name, image.String()),
+	)
 	return nil
 }
 
@@ -65,6 +81,25 @@ func (e *ensurer) EnsureMachineControllerManagerVPA(_ context.Context, _ gcontex
 	if !e.gardenletManagesMCM {
 		return nil
 	}
+
+	var (
+		minAllowed = corev1.ResourceList{
+			corev1.ResourceMemory: resource.MustParse("64Mi"),
+		}
+		maxAllowed = corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse("2"),
+			corev1.ResourceMemory: resource.MustParse("5G"),
+		}
+	)
+
+	if newObj.Spec.ResourcePolicy == nil {
+		newObj.Spec.ResourcePolicy = &vpaautoscalingv1.PodResourcePolicy{}
+	}
+
+	newObj.Spec.ResourcePolicy.ContainerPolicies = extensionswebhook.EnsureVPAContainerResourcePolicyWithName(
+		newObj.Spec.ResourcePolicy.ContainerPolicies,
+		machinecontrollermanager.ProviderSidecarVPAContainerPolicy(gcp.Name, minAllowed, maxAllowed),
+	)
 	return nil
 }
 
