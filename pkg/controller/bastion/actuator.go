00b65dbc23f761cb33498ad72c91efa71d0915dc
Alexander Hebel <a.hebelsun@gmail.com>
2024-08-05 15:00:05 +0200

    Remove redundant import alias

--- pkg/controller/bastion/actuator.go
+++ pkg/controller/bastion/actuator.go
@@ -16,7 +16,7 @@ import (
 	"github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	"github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/go-logr/logr"
-	compute "google.golang.org/api/compute/v1"
+	"google.golang.org/api/compute/v1"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
