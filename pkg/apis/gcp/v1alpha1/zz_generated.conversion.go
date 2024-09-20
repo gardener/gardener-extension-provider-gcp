985bec808f3ec8e50485cbcc289b79e628c05811
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2024-09-20 19:45:24 +0100

    add infrastructure status

--- pkg/apis/gcp/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/gcp/v1alpha1/zz_generated.conversion.go
@@ -812,6 +812,7 @@ func autoConvert_v1alpha1_NetworkStatus_To_gcp_NetworkStatus(in *NetworkStatus, 
 	}
 	out.Subnets = *(*[]gcp.Subnet)(unsafe.Pointer(&in.Subnets))
 	out.NatIPs = *(*[]gcp.NatIP)(unsafe.Pointer(&in.NatIPs))
+	out.DualStackEnabled = in.DualStackEnabled
 	return nil
 }
 
@@ -826,6 +827,7 @@ func autoConvert_gcp_NetworkStatus_To_v1alpha1_NetworkStatus(in *gcp.NetworkStat
 	}
 	out.Subnets = *(*[]Subnet)(unsafe.Pointer(&in.Subnets))
 	out.NatIPs = *(*[]NatIP)(unsafe.Pointer(&in.NatIPs))
+	out.DualStackEnabled = in.DualStackEnabled
 	return nil
 }
 
