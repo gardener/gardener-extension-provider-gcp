d8491ce20acbf7154f7a84914e98fc9883fc9f60
Yerasala Venkata, Seshachalam <seshachalam.yerasala.venkata@sap.com>
2024-11-29 09:46:39 +0530

    Add BackupBucketConfig and ImmutableConfig types to GCP provider
    - Introduced BackupBucketConfig and ImmutableConfig types in both internal and v1alpha1 API versions.
    - Registered the new types in the scheme.
    - `make generate` to update the generated code.

---
+++ pkg/apis/gcp/v1alpha1/types_backupbucket.go
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// BackupBucketConfig represents the configuration for a backup bucket.
+type BackupBucketConfig struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Immutability defines the immutability config for the backup bucket.
+	Immutability ImmutableConfig `json:"immutability"`
+}
+
+// ImmutableConfig represents the immutability configuration for a backup bucket.
+type ImmutableConfig struct {
+	// RetentionType specifies the type of retention for the backup bucket.
+	// Currently allowed values are:
+	// - "bucket": The retention policy applies to the entire bucket.
+	RetentionType string `json:"retentionType"`
+
+	// RetentionPeriod specifies the immutability retention period for the backup bucket.
+	// The minimum retention period is 24 hours as per Google Cloud Storage requirements.
+	// Reference: https://github.com/googleapis/google-cloud-go/blob/3005f5a86c18254e569b8b1782bf014aa62f33cc/storage/bucket.go#L1430-L1434
+	RetentionPeriod metav1.Duration `json:"retentionPeriod"`
+
+	// Locked indicates whether the immutable retention policy is locked for the backup bucket.
+	// If set to true, the retention policy cannot be removed or the retention period reduced, enforcing immutability.
+	Locked bool `json:"locked"`
+}
