186785dc283442d3fe814910973df2f214c917e5
Alexander Hebel <a.hebelsun@gmail.com>
2025-04-23 17:48:26 +0200

    Adjust unit tests

--- pkg/apis/gcp/validation/worker.go
+++ pkg/apis/gcp/validation/worker.go
@@ -25,7 +25,7 @@ var (
 )
 
 // ValidateWorkerConfig validates a WorkerConfig object.
-func ValidateWorkerConfig(workerConfig gcp.WorkerConfig, dataVolumes []core.DataVolume, fldPath *field.Path) field.ErrorList {
+func ValidateWorkerConfig(workerConfig gcp.WorkerConfig, dataVolumes []core.DataVolume, bootVolume *core.Volume, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
 	for idx, dataVolume := range dataVolumes {
@@ -47,6 +47,10 @@ func ValidateWorkerConfig(workerConfig gcp.WorkerConfig, dataVolumes []core.Data
 	allErrs = append(allErrs, validateNodeTemplate(workerConfig.NodeTemplate, fldPath.Child("nodeTemplate"))...)
 	if workerConfig.DataVolumes != nil {
 		allErrs = append(allErrs, validateDataVolumeConfigs(dataVolumes, workerConfig.DataVolumes, fldPath.Child("dataVolumes"))...)
+	}
+
+	if bootVolume != nil && bootVolume.Type != nil && workerConfig.BootVolume != nil {
+		allErrs = append(allErrs, validateBootVolumeConfig(*workerConfig.BootVolume, *bootVolume.Type, fldPath.Child("volume"))...)
 	}
 
 	if workerConfig.MinCpuPlatform != nil {
@@ -179,24 +183,31 @@ func validateDataVolumeConfigs(dataVolumes []core.DataVolume, configs []gcp.Data
 			continue
 		}
 		volumeNames.Insert(volumeName)
-		allErrs = append(allErrs, validateHyperDisk(dataVolumes[idx], configDataVolume, fldPath)...)
+		if dataVolumes[idx].Type != nil {
+			allErrs = append(allErrs, validateHyperDisk(
+				configDataVolume.DiskSettings, *dataVolumes[idx].Type, fldPath)...)
+		}
 	}
 
 	return allErrs
 }
 
-func validateHyperDisk(dataVolume core.DataVolume, config gcp.DataVolume, fldPath *field.Path) field.ErrorList {
+func validateHyperDisk(config gcp.DiskSettings, volumeType string, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	if config.ProvisionedIops != nil && !slices.Contains(worker.AllowedTypesIops, *dataVolume.Type) {
+	if config.ProvisionedIops != nil && !slices.Contains(worker.AllowedTypesIops, volumeType) {
 		allErrs = append(allErrs, field.Forbidden(fldPath.Child("provisionedIops"),
 			fmt.Sprintf("is only allowed for types: %v", worker.AllowedTypesIops)))
 	}
-	if config.ProvisionedThroughput != nil && !slices.Contains(worker.AllowedTypesThroughput, *dataVolume.Type) {
+	if config.ProvisionedThroughput != nil && !slices.Contains(worker.AllowedTypesThroughput, volumeType) {
 		allErrs = append(allErrs, field.Forbidden(fldPath.Child("provisionedThroughput"),
 			fmt.Sprintf("is only allowed for types: %v", worker.AllowedTypesThroughput)))
 	}
 	return allErrs
+}
+
+func validateBootVolumeConfig(config gcp.BootVolume, volumeType string, fldPath *field.Path) field.ErrorList {
+	return validateHyperDisk(config.DiskSettings, volumeType, fldPath)
 }
 
 func validateNodeTemplate(nt *extensionsv1alpha1.NodeTemplate, fldPath *field.Path) field.ErrorList {
