3f3d80314c0c9afce0ded605ff7a4d0614f37cfe
georgibaltiev <georgi.baltiev@sap.com>
2025-05-14 17:07:39 +0300

    Specify the extension's next version

--- cmd/gardener-extension-provider-gcp/app/app.go
+++ cmd/gardener-extension-provider-gcp/app/app.go
@@ -256,7 +256,7 @@ func NewControllerManagerCommand(ctx context.Context) *cobra.Command {
 				return fmt.Errorf("could not add ready check for webhook server to manager: %w", err)
 			}
 
-			// TODO (georgibaltiev): remove after the extension's next release
+			// TODO (georgibaltiev): Remove after the release of version 1.44.0
 			log.Info("Adding migration runnables")
 			if err := mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
 				return purgeMachineControllerManagerRBACResources(ctx, mgr.GetClient())
