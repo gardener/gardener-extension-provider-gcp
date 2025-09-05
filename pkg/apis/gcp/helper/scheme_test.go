ca7be4816b4e6df3679c63538b1a4d1b1a8a27b5
vpnachev <vladimir.nachev@sap.com>
2025-09-05 18:12:42 +0300

    Add util function to parse WorkloadIdentityConfig from runtime.RawExtension

---
+++ pkg/apis/gcp/helper/scheme_test.go
@@ -0,0 +1,148 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package helper_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/gardener/gardener-extension-provider-gcp/pkg/apis/gcp/helper"
+)
+
+var _ = Describe("Scheme", func() {
+	Describe("WorkloadIdentityConfigFromBytes", func() {
+		It("should successfully parse WorkloadIdentityConfig", func() {
+			raw := []byte(`apiVersion: gcp.provider.extensions.gardener.cloud/v1alpha1
+kind: WorkloadIdentityConfig
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+`)
+			config, err := helper.WorkloadIdentityConfigFromBytes(raw)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(config).ToNot(BeNil())
+			Expect(config.ProjectID).To(Equal("my-project"))
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due to nil config", func() {
+			config, err := helper.WorkloadIdentityConfigFromBytes(nil)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("cannot parse WorkloadIdentityConfig from empty config"))
+			Expect(config).To(BeNil())
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due to empty config", func() {
+			config, err := helper.WorkloadIdentityConfigFromBytes([]byte{})
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("cannot parse WorkloadIdentityConfig from empty config"))
+			Expect(config).To(BeNil())
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due to unknown field", func() {
+			raw := []byte(`apiVersion: gcp.provider.extensions.gardener.cloud/v1alpha1
+kind: WorkloadIdentityConfig
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+additionalField: additionalValue
+`)
+			config, err := helper.WorkloadIdentityConfigFromBytes(raw)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("strict decoding error: unknown field \"additionalField\""))
+			Expect(config).To(BeNil())
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due to missing apiVersion", func() {
+			raw := []byte(`kind: WorkloadIdentityConfig
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+`)
+			config, err := helper.WorkloadIdentityConfigFromBytes(raw)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(ContainSubstring("Object 'apiVersion' is missing in")))
+			Expect(config).To(BeNil())
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due to unsupported apiVersion", func() {
+			raw := []byte(`apiVersion: gcp.provider.extensions.gardener.cloud/v0
+kind: WorkloadIdentityConfig
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+`)
+			config, err := helper.WorkloadIdentityConfigFromBytes(raw)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(ContainSubstring("no kind \"WorkloadIdentityConfig\" is registered for version \"gcp.provider.extensions.gardener.cloud/v0\" in scheme")), err.Error())
+			Expect(config).To(BeNil())
+		})
+
+		It("should fail to parse WorkloadIdentityConfig due unregistered kind", func() {
+			raw := []byte(`apiVersion: gcp.provider.extensions.gardener.cloud/v1alpha1
+kind: FooBar
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+`)
+			config, err := helper.WorkloadIdentityConfigFromBytes(raw)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(ContainSubstring("no kind \"FooBar\" is registered for version \"gcp.provider.extensions.gardener.cloud/v1alpha1\" in scheme")))
+			Expect(config).To(BeNil())
+		})
+	})
+
+	Describe("WorkloadIdentityConfigFromRaw", func() {
+		It("should should fail to parse WorkloadIdentityConfig due to nil raw", func() {
+			config, err := helper.WorkloadIdentityConfigFromRaw(nil)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("cannot parse WorkloadIdentityConfig from empty RawExtension"))
+			Expect(config).To(BeNil())
+		})
+
+		It("should should fail to parse WorkloadIdentityConfig due to nil raw", func() {
+			config, err := helper.WorkloadIdentityConfigFromRaw(&runtime.RawExtension{Raw: nil})
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("cannot parse WorkloadIdentityConfig from empty RawExtension"))
+			Expect(config).To(BeNil())
+		})
+
+		It("should successfully parse WorkloadIdentityConfig", func() {
+			raw := &runtime.RawExtension{Raw: []byte(`apiVersion: gcp.provider.extensions.gardener.cloud/v1alpha1
+kind: WorkloadIdentityConfig
+projectID: "my-project"
+credentialsConfig:
+  universe_domain: "googleapis.com"
+  type: "external_account"
+  audience: "//iam.googleapis.com/projects/11111111/locations/global/workloadIdentityPools/foopool/providers/fooprovider"
+  subject_token_type: "urn:ietf:params:oauth:token-type:jwt"
+  token_url: "https://sts.googleapis.com/v1/token/new"
+`)}
+			config, err := helper.WorkloadIdentityConfigFromRaw(raw)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(config.ProjectID).To(Equal("my-project"))
+		})
+	})
+})
